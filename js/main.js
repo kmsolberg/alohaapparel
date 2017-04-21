3d87379bb34673a5cf6989f208b7d422de190c77
Kat Solberg <katsolberg@gmail.com>
2017-04-21 16:41:48 -0700

    carousel formatting

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(function (){
+
+// code between these lines!
+
+// FLICKITY SLIDER
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'center',
+    contain: true,
+    wrapAround: true
+  });
+
+// SMOOTH SCROLLING (CSS TRICKS)
+
+// FORM SUBMIT 
+
+
+
+});
